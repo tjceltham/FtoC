c9aaa7effb0198c5ef474966d6d2da4d17f87fdd
Tony Collins <tjc@eltham-college.org.uk>
2023-02-17 16:34:22 +0000

    Add project files.

---
+++ FtoC/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FtoC
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void input_Click(object sender, RoutedEventArgs e)
+        {
+            double value = 0;
+            // read value from input text box, convert to a double and store in value
+            value = Convert.ToDouble(input.Text);
+            // multiply value by 10
+            value = value * 10;
+            // Outputs the value to the output text box
+            output.Text = Convert.ToString(value);
+           
+            // changing the background colour of the multiply button
+            // any property can be changed
+            mul.Background = Brushes.Blue;
+
+    
+        }
+    }
+}
